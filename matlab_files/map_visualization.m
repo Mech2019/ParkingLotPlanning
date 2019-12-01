217e3bb2445b2c9c5c0935191b67f7df1b1bcf39
yanjunp-cmu <yanjunp@andrew.cmu.edu>
2019-12-01 16:20:53 -0500

    Added Matlab visulaization and moved all *.m to matlab_files

---
+++ matlab_files/map_visualization.m
@@ -0,0 +1,35 @@
+map = load('../src/map.csv');
+
+%% visualize
+% parking lot map
+figure()
+for i = 1:total_slot_num
+    % draw rectangle
+    rectangle('Position',[map(i,1) - slot_depth/2, ...
+        map(i,2) - slot_width/2, slot_depth, slot_width]);
+    hold on;
+    if (map(i,4) == 1)
+        rectangle('Position',[map(i,1) - car_len/2, ...
+            map(i,2) - car_wid/2, car_len, car_wid], ...
+            'FaceColor', 'b');
+        hold on;
+    end
+end
+% plot ego vehicle for line of sight test!!!
+% car position 1
+rectangle('Position',[8.5 - car_wid/2, ...
+            2.75 - car_len/2, car_wid, car_len], ...
+            'FaceColor', 'r');
+hold on;
+draw_circle(8.5, 2.75, sensor_rad);
+hold on;
+% car position 2
+rectangle('Position',[25.5 - car_wid/2, ...
+    10.75 - car_len/2, car_wid, car_len], ...
+    'FaceColor', 'r');
+hold on;
+draw_circle(25.5, 10.75, sensor_rad);
+hold off;
+axis equal
+xlim([0 parking_lot_width]);ylim([0 parking_lot_length]);
+% imagesc(lot); axis equal; colorbar; colormap jet; hold on;
--- matlab_files/scenario_gen_proposal.m
+++ matlab_files/scenario_gen_proposal.m
@@ -119,39 +119,5 @@ slot_xy_flag(29,4) = 0;
 slot_xy_flag(40,4) = 0;
 slot_xy_flag(46,4) = 0;
 
-%% visualize
-% parking lot map
-figure()
-for i = 1:total_slot_num
-    % draw rectangle
-    rectangle('Position',[slot_xy_flag(i,1) - slot_depth/2, ...
-        slot_xy_flag(i,2) - slot_width/2, slot_depth, slot_width]);
-    hold on;
-    if (slot_xy_flag(i,4) == 1)
-        rectangle('Position',[slot_xy_flag(i,1) - car_len/2, ...
-            slot_xy_flag(i,2) - car_wid/2, car_len, car_wid], ...
-            'FaceColor', 'b');
-        hold on;
-    end
-end
-% plot ego vehicle for line of sight test!!!
-% car position 1
-rectangle('Position',[8.5 - car_wid/2, ...
-            2.75 - car_len/2, car_wid, car_len], ...
-            'FaceColor', 'r');
-hold on;
-draw_circle(8.5, 2.75, sensor_rad);
-hold on;
-% car position 2
-rectangle('Position',[25.5 - car_wid/2, ...
-    10.75 - car_len/2, car_wid, car_len], ...
-    'FaceColor', 'r');
-hold on;
-draw_circle(25.5, 10.75, sensor_rad);
-hold off;
-axis equal
-xlim([0 parking_lot_width]);ylim([0 parking_lot_length]);
-% imagesc(lot); axis equal; colorbar; colormap jet; hold on;
-
 %% data output
-csvwrite('./src/map.csv', slot_xy_flag);
+csvwrite('../src/map.csv', slot_xy_flag);
