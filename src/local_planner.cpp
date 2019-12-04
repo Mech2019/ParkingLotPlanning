9fa805b1ec6bdd3f0771c05c82fb6bcc7ba4b3a8
jingzhao <jingzhao@andrew.cmu.edu>
2019-12-03 19:22:47 -0500

    add path reconstruction

--- src/local_planner.cpp
+++ src/local_planner.cpp
@@ -8,6 +8,7 @@ RRT_Tree::RRT_Tree(CarState start, CarState goal) {
   graph[0] = {};
   size = 1;
   min_dist = INT_MAX;
+  reached = 0;
 }
 
 void RRT_Tree::sample_node(int id, CarState& rand_state) {
@@ -15,9 +16,22 @@ void RRT_Tree::sample_node(int id, CarState& rand_state) {
 
   double rand_x, rand_y, rand_theta;
 
-  rand_x = rand() % MAP_WIDTH;
-  rand_y = rand() % MAP_HEIGHT;
-  rand_theta = rand() % (int) (2 * M_PI / RANDOM_STEP) * RANDOM_STEP;
+  int rand_for_bias = rand() % 100;
+
+  if (rand_for_bias < (GOAL_BIAS * 100)) {
+
+    rand_x = goal_pos.get_x();
+    rand_y = goal_pos.get_y();
+    rand_theta =  goal_pos.get_theta();
+    cout << "BIASED!" << endl;
+
+  } else {
+    rand_x = rand() % MAP_WIDTH;
+    rand_y = rand() % MAP_HEIGHT;
+    rand_theta = rand() % (int) (2 * M_PI / RANDOM_STEP) * RANDOM_STEP;
+  }
+
+
 
   rand_state.set_x(rand_x);
   rand_state.set_y(rand_y);
@@ -106,10 +120,12 @@ void RRT_Tree::extend(CarState& rand_state) {
   node_map[new_id] = new_state;
   graph[nearest_id].push_back(new_id);
   graph[new_id] = {};
+  parent_node[new_id] = nearest_id;
   size++;
 
   // check if reached
   if (check_if_reached(new_state, goal_pos)){
+    reached = 1;
     cout << "reached!" << endl;
   }
 
@@ -174,7 +190,30 @@ bool RRT_Tree::check_if_reached(CarState& from, CarState& goal){
   return false;
 }
 
+void RRT_Tree::reconstruct_path(int last_node_id, vector<int> &path_id) {
+  if (last_node_id == 0) {
+    cout << "already in goal position with threshold setting!\n";
+  }
 
+  // push back the given node id
+  path_id.push_back(last_node_id);
+
+  int parent_id = parent_node[last_node_id];
+
+  while (parent_id != 0) {
+    path_id.push_back(parent_id);
+    parent_id = parent_node[parent_id];
+  }
+//    path_id.push_back(0);
+  reverse(begin(path_id), end(path_id));
+
+  cout << "reconstruct path is: ";
+  for (auto v : path_id){
+    cout << v << ", ";
+  }
+  cout << endl;
+
+}
 
 
 
@@ -187,9 +226,25 @@ void local_planner(CarState &start, CarState &goal,
   cout << "start " << start << endl;
   cout << "goal " << goal << endl;
 
+  // open a file in write mode.
+  ofstream outfile;
+  outfile.open("local_result.txt");
+  outfile << start << endl;
+
+  bool reached = 0;
+  vector<int> path_id;
+
   RRT_Tree tree(start, goal);
   for (int i = 1; i<1000; i++){
     tree.add_node(i);
+    if (tree.reached){
+      tree.reconstruct_path(i, path_id);
+      break;
+    }
+  }
+
+  for (auto id : path_id){
+    outfile << tree.node_map[id] << endl;
   }
 
 
@@ -199,22 +254,14 @@ void local_planner(CarState &start, CarState &goal,
 //  tree.print_tree();
 
 
-
-
-
-
-
-
-
-  // open a file in write mode.
-  ofstream outfile;
-  outfile.open("local_result.txt");
-  outfile << start << endl;
-
   outfile << goal << endl;
   outfile.close();
 
 
 }
+
+
+
+
 
 
--- src/local_planner.h
+++ src/local_planner.h
@@ -21,10 +21,12 @@
 const double EPSILON_DIST = 0.5;
 const double EPSILON_THETA = M_PI / 10;
 const double RANDOM_STEP = M_PI / 20;
-const double GOAL_THRESHOLD = 1.0;
+const double GOAL_THRESHOLD = 0.2;
+const double GOAL_BIAS = 0.05;
 
 void local_planner(CarState &start_state, CarState &goal_state,
                    vector<CarState>& plan);
+
 
 class RRT_Tree {
 public:
@@ -33,7 +35,9 @@ public:
   CarState goal_pos;
   unordered_map<int, vector<int>> graph;
   unordered_map<int, CarState> node_map;
+  unordered_map<int, int> parent_node;
   double min_dist;
+  bool reached;
 
   RRT_Tree(CarState start, CarState goal);
   void add_node(int id);
@@ -49,8 +53,9 @@ public:
   void print_tree();
   double calculate_distance(CarState& from_state, CarState& to_state);
   bool check_if_reached(CarState& from, CarState& goal);
-
+  void reconstruct_path(int last_node_id, vector<int> &path_id);
 };
+
 
 
 #endif //PARKINGLOTPLANNING_SRC_LOCAL_PLANNER_H
